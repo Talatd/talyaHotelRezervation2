1a14cee5fa5afc153c291d6432a083dfc886ffd5
Talatd <ttalattdemir@gmail.com>
2025-08-05 13:24:32 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,38 +3,38 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { hotelData } from "@/../hotel-data.json"; 
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer"; 
+import "react-day-picker/style.css";
+import { getStaticHotelData } from "@/lib/static-data"; // Yeni fonksiyonu import edin
 
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export async function generateMetadata(): Promise<Metadata> {
+  const hotelData = await getStaticHotelData();
 
-export const metadata: Metadata = {
-  title: hotelData["seo-social-media"]["seo-title"],
-  description: hotelData["seo-social-media"]["seo-description"],
-};
+  return {
+    title: hotelData["seo-social-media"]["seo-title"],
+    description: hotelData["seo-social-media"]["seo-description"],
+  };
+}
 
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const hotelData = await getStaticHotelData();
+
   return (
     <html lang={hotelData.general["default-language"].toLowerCase()}>
       <body className={inter.className}>
-        {}
-        <Navbar />
-
-        {}
+        <Navbar hotelName={hotelData.general.name} logoUrl={hotelData.footer["logo-url"]} />
         <main className="bg-background text-foreground">
           {children}
         </main>
-
-        {}
-        <Footer />
+        <Footer hotelData={hotelData} />
       </body>
     </html>
   );
